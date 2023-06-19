43d99019f5d6d33f32e94b389eede334d6cde458
ALakshmi123 <adilakshmiyella1999@gmail.com>
2023-06-19 17:56:44 +0530

    Review comments changes

--- weatherApp/weatherApp.js
+++ weatherApp/weatherApp.js
@@ -6,48 +6,78 @@ let degress = document.getElementById("temperature");
 let btn = document.getElementById("get-weather");
 let icon = document.getElementById("cloud-img");
 let cardContainer = document.getElementById("weather-cards");
+let errorMessage = document.getElementById("error-text");
+var modalPopUp = document.getElementById("my-modal");
+var closeIcon = document.getElementsByClassName("close")[0];
 
 const apiKey = "RGLWMCQF2M8HJUNW5BWFHTQF5";
 
 //Fetching the API response of today and next 1 week weather report on click of Get Weather Button
 btn.onclick = function() {
+  let letters = /^[A-Z a-z]+$/;
     if( locationInput.value === "" ) {
-        alert("Please enter a city name");
+      modalPopUp.style.display = "block";
+      errorMessage.innerHTML = "Please enter a city name excluding special characters. For example: Hyderabad";
+      // When the user clicks the modal
+      closeIcon.onclick = function() {
+        modalPopUp.style.display = "none";
+      }
+    }
+    else if( locationInput.value.match(letters) )  {
+      getWeather();
     }
     else {
-      let str="";
-      let city = locationInput.value;
-      let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`;
-      fetch(url, {
-          "method": "GET"
-      })
-      .then(response => {
-          return response.json();
-      })
-      .then(data => {
-          degress.innerHTML = data.days[0].temp+`<span class="id">&nbsp;&deg;</span><span>&nbsp;C</span>`;
-          description.innerHTML = data.days[0].conditions;
-          icon.innerHTML = getIcon(data.days[0].icon);
-          locationName.innerHTML = data.address;
-          weatherCards = data.days.slice(1,8);
-          weatherCards.forEach((ele,index) => {
-          str +=
-          `<div class="container-block" key=${ele.datetime}>
-          <div>${getDayName(ele.datetime)}</div>
-            <div> ${getIcon(ele.icon)}</div>
-              <span>${ele.temp}</span><span class="id"> &deg;</span><span> C</span>
-              <div>${ele.description}</div>
-          </div>`
-          })
-          cardContainer.innerHTML = str;
-      })
-      .catch(err => {
-        if(err) {
-          alert("Please enter a valid city name");
-        }
-      });
-  }  
+      modalPopUp.style.display = "block";
+      errorMessage.innerHTML = "Please enter alphabets only, special characters and numbers are not allowed";
+      // When the user clicks the modal
+      closeIcon.onclick = function() {
+        modalPopUp.style.display = "none";
+      }
+    }
 }
+
+function getWeather() {
+  let str="";
+  let city = locationInput.value;
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`;
+  fetch(url, {
+      "method": "GET"
+  })
+  .then(response => {
+      return response.json();
+  })
+  .then(data => {
+    if(data.length!==0 && data.address.length === city.length) {
+      errorMessage.innerHTML = "";
+      degress.innerHTML = data.days[0].temp+`<span class="id">&nbsp;&deg;</span><span>&nbsp;C</span>`;
+      description.innerHTML = data.days[0].conditions;
+      icon.innerHTML = getIcon(data.days[0].icon);
+      locationName.innerHTML = data.address;
+      weatherCards = data.days.slice(1,8);
+      weatherCards.forEach((ele,index) => {
+      str +=
+      `<div class="container-block" key=${ele.datetime}>
+      <div>${getDayName(ele.datetime)}</div>
+        <div> ${getIcon(ele.icon)}</div>
+          <span>${ele.temp}</span><span class="id"> &deg;</span><span> C</span>
+          <div>${ele.description}</div>
+      </div>`
+      })
+      cardContainer.innerHTML = str;
+    }
+  })
+  .catch(err => {
+    if(err) {
+      errorMessage.innerHTML = "Please enter a valid city name. For example: Hyderabad, New York.";
+      modalPopUp.style.display = "block";
+      // When the user clicks the modal
+      closeIcon.onclick = function() {
+        modalPopUp.style.display = "none";
+      }
+      }
+  });
+}
+
 
 //getIcon(): this method used for getting the icons based on weather condition
 function getIcon(condition) {
@@ -81,4 +111,5 @@ function getDayName(date) {
     ];
     return days[day.getDay()];
 }
+
 
